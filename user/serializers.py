647f569a6ff2eb566534e2c0d77e01d960033784
twonumberfortyfives <sma4no.7@gmail.com>
2024-09-14 00:30:01 +0300

    implemented safe registration

--- user/serializers.py
+++ user/serializers.py
@@ -9,3 +9,15 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ("username", "first_name", "last_name", "email", "password")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
+    def create(self, validated_data):
+        password = validated_data["password"]
+        if password:
+            user = User.objects.create_user(
+                username=validated_data["username"],
+                first_name=validated_data["first_name"],
+                last_name=validated_data["last_name"],
+                email=validated_data["email"],
+            )
+            user.set_password(password)
+            user.save()
+            return user
