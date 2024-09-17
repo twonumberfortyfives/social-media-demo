e6f2c0ce0786ec5cd23bdd05ff4f855e3cc4fce8
twonumberfortyfives <sma4no.7@gmail.com>
2024-09-17 08:01:08 +0300

    foundation for user admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register()
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
