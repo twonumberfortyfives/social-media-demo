79af3f7536c73232fe693e5127db1f1a96ef5332
twonumberfortyfives <sma4no.7@gmail.com>
2024-09-17 09:32:08 +0300

    implemented validation for follow service

--- follow/models.py
+++ follow/models.py
@@ -1,9 +1,26 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from user.models import User
 
 
 class Follow(models.Model):
-    following_user_id = models.ManyToManyField(User, related_name='followers_following')
-    followed_user_id = models.ManyToManyField(User, related_name='followers_followed')
+    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE, null=False, blank=False)
+    followed = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (('follower', 'followed'),)
+
+    @staticmethod
+    def validate_follow_yourself(following_user, followed_user):
+        equal = followed_user == following_user
+        return equal
+
+    def clean(self):
+        if self.validate_follow_yourself(self.follower, self.followed):
+            raise ValidationError('You cannot follow yourself!')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(Follow, self).save(*args, **kwargs)
--- follow/serializers.py
+++ follow/serializers.py
@@ -7,3 +7,12 @@ class FollowListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Follow
         fields = '__all__'
+
+    def validate(self, data):
+        common = Follow.validate_follow_yourself(data["follower"], data["followed"])
+        if common:
+            raise serializers.ValidationError(
+                "You can not follow yourself."
+            )
+        return data
+
